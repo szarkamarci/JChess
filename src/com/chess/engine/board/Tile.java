f8360cdc938fc3bbbec654260dc77d35a7e3bc8b
szarkamarci <heavymarcy98@gmail.com>
2021-10-18 13:32:49 +0200

    Basics

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,21 +1,43 @@
+package com.chess.engine.board;
 
+import com.google.common.collect.ImmutableMap; // guava import
 import com.chess.engine.pieces.Piece;
 
-public class Tile {
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
 
     protected final int tileCoordinate;
+
+    //Adding integers to emptytiles
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
+
+    //Generate Empty tiles
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer,EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++){
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap); //ImmutableMap is blocking to change "emptyTileMap"
+    }
+
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
 
     //constructor
     private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
-    //abstract methods
     public abstract boolean isTileOccupied();
 
     public abstract Piece getPiece();
 
-    //EmptyTile Class
     public static final class EmptyTile extends Tile{
 
         //constructor
@@ -34,7 +56,6 @@ public class Tile {
         }
     }
 
-    //OccupiedTile Class
     public static final class OccupiedTile extends Tile{
 
         private final Piece pieceOnTile;
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,4 +1,2 @@
 package com.chess.engine.pieces;
 
-public class Piece {
-}
