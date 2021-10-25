b1121337b48e9ec4609ba67ebc3b26cd01afadf9
szarkamarci <heavymarcy98@gmail.com>
2021-10-25 18:53:57 +0200

    Pieces

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,2 +1,28 @@
 package com.chess.engine.pieces;
 
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.Collection;
+
+public abstract class Piece {
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
+     this.pieceAlliance = pieceAlliance;
+     this.piecePosition = piecePosition;
+     //TODO more work here
+     this.isFirstMove = false;
+    }
+
+    public Alliance getPieceAlliance(){
+        return this.pieceAlliance;
+    }
+
+    public boolean isFirstMove(){
+        return this.isFirstMove;
+    }
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
+}
